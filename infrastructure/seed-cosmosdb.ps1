f7f6747b06a96f6dacb514be5b478ba3a10f6fa7
Maciek <maciejekbham@interia.pl>
2022-03-27 18:40:46 +0000

    Initial commit

---
+++ infrastructure/seed-cosmosdb.ps1
@@ -0,0 +1,17 @@
+$studentprefix = "Your 3 letter abbreviation here"
+$githubAccount = "Your github account name here"
+$resourcegroupName = "fabmedical-rg-" + $studentprefix
+$cosmosDBName = "fabmedical-cdb-" + $studentprefix
+
+# Fetch CosmosDB Mongo connection string
+$mongodbConnectionString = `
+    $(az cosmosdb keys list `
+        --name $cosmosDBName `
+        --resource-group $resourcegroupName `
+        --type connection-strings `
+        --query 'connectionStrings[0].connectionString')
+
+# Seed CosmosDB database
+docker run -ti `
+    -e MONGODB_CONNECTION="$mongodbConnectionString" `
+    ghcr.io/$githubAccount/fabrikam-init:main
